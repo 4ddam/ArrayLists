f529e4672a60286859904490aac104d1be1ac071
Adam Browning <adambrowning02@gmail.com>
2020-02-05 18:57:29 -0500

    need to fix the first input

--- Hangman.java
+++ Hangman.java
@@ -17,17 +17,18 @@ public class Hangman
     public static void main() {
         int guessesLeft = 10;
         String stringWord;
-        char guess;
-        boolean win = false;
+        String guess;
+        boolean end = false;
 
         ArrayList<Character> word = new ArrayList<Character>();
         ArrayList<Character> guessedLetters = new ArrayList<Character>();
+        ArrayList<Character> display = new ArrayList<Character>();
 
         Scanner input = new Scanner (System.in);
 
         System.out.println("\f        HANGMAN\n\n");
 
-        System.out.print("Player 1 -  Enter the word for player 2 to guess: ");
+        System.out.print("Enter a word: ");
         stringWord = input.nextLine().toLowerCase();
 
         while (true) {
@@ -40,8 +41,77 @@ public class Hangman
                 System.out.print("\f        HANGMAN\n\nThe word can only contain letters and spaces\n");
             }
 
-            System.out.print("Player 1 -  Enter the word for player 2 to guess: ");
+            System.out.print("Enter a word: ");
             stringWord = input.nextLine().toLowerCase();
+        }
+
+        for (int i = 0; i < stringWord.length(); i++) {
+            if (stringWord.charAt(i) == ' '){
+                display.add(' ');
+            } else {
+                display.add('_');
+            }
+            word.add(stringWord.charAt(i));
+        }        
+
+        System.out.print("\f        HANGMAN\n\nGame Started\n\n");
+
+        while (end == false) {  
+            System.out.print("Guesses Left: " + guessesLeft + "\nLetters Guessed: ");
+
+            for (char letters: guessedLetters) {
+                System.out.print(letters + ", ");
+            }
+
+            System.out.print("\n\n");
+
+            for (char character: display) {
+                System.out.print(character + " ");
+            }
+
+            System.out.print("\n\nGuess a letter: ");
+            guess = input.nextLine();
+            if (guess.length() == 1 && (guess.matches("([a-zA-Z])")) && !(guessedLetters.contains(guess.charAt(0)))) {
+                if (word.contains(guess.charAt(0))) {
+                    for (int i = 0; i < stringWord.length(); i++) {
+                        if (word.get(i) == guess.charAt(0)) {
+                            display.set(i, guess.charAt(0));                             
+                        } 
+                    }
+                    System.out.print("\f        HANGMAN\n\nCorrect Guess\n\n");
+                } else {
+                    guessesLeft--;                    
+                    if (guessesLeft < 1) {
+                        end = true;
+                    }                    
+                    System.out.print("\f        HANGMAN\n\nIncorrect Guess\n\n");
+                }
+                guessedLetters.add(guess.charAt(0));               
+            } else {
+                System.out.print("\f        HANGMAN\n\nNot a valid guess. That is already guessed or that is not one letter\n\n");
+            }
+            if (display.equals(word)) {
+                end = true;
+            }   
+        }
+
+        System.out.print("Guesses Left: " + guessesLeft + "\nLetters Guessed: ");
+
+        for (char letters: guessedLetters) {
+            System.out.print(letters + ", ");
+        }
+
+        System.out.print("\n\n");
+
+        for (char character: display) {
+            System.out.print(character + " ");
+        }
+
+        if (guessesLeft < 1) {
+            System.out.print("\n\nYou ran out of guesses!");
+        } else {
+            System.out.print("\n\nYou win!");
         }
     }
 }
+
