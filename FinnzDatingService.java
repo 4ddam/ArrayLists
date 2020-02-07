cdebcec1e535e2d6cc9c6ee7c1e4ec4651486c3a
4ddam <adambrowning02@gmail.com>
2020-02-07 13:09:20 -0500

    finalized dating service

--- FinnzDatingService.java
+++ FinnzDatingService.java
@@ -20,39 +20,36 @@ public class FinnzDatingService
         ArrayList<String> nameList = new ArrayList<String>();        
         Scanner input = new Scanner(System.in);
         String response = "";
+        String numCousins = "";
 
         System.out.print("\f      FINNZ DATING SERVICE\n\n");
 
         while (true) {
             System.out.print("Finn, how many cousins do you have? ");
-            response = input.nextLine();
-            if (response.matches("^[0-9]*$")) {
+            numCousins = input.nextLine();
+            if (numCousins.matches("^[0-9]*$")) {
                 break;
             } else {
-                System.out.print("\f        FINNZ DATING SERVICE\n\nYou can only input a number!\n\n");
+                System.out.print("\f      FINNZ DATING SERVICE\n\nYou can only input a number!\n\n");
             }
         }
 
         System.out.print("\f      FINNZ DATING SERVICE\n\n");
 
-        for (int i = 0; i < Integer.parseInt(response); i++) {
+        for (int i = 0; i < Integer.parseInt(numCousins); i++) {
             while (true) {
                 System.out.print("What is the name of cousin #" + (i+1) + "? ");
                 String response1 = input.nextLine();
-                if (response1.toLowerCase().matches("^[\\p{L} .'-]+$")) {
+                if (response1.toLowerCase().matches("^[\\p{L} .'-]+$") && !(nameList.contains(response1))) {
                     nameList.add(response1.toLowerCase());
                     System.out.print("\f      FINNZ DATING SERVICE\n\n");
                     break;
                 } else {
-                    System.out.print("\f        FINNZ DATING SERVICE\n\nThe name can only contain letters!\n\n");
+                    System.out.print("\f      FINNZ DATING SERVICE\n\nThe name can only be letters and cannot be on the list!\n\n");
                 }   
-
             }
         }
-
-        System.out.print("\f      FINNZ DATING SERVICE\n\nList of names:\n");
-        printArray(nameList);      
-        System.out.println();
+        printInfo(nameList);  
 
         while (true) {
             System.out.print("Finn, who is your best looking cousin? ");
@@ -61,16 +58,12 @@ public class FinnzDatingService
                 nameList.remove(response);
                 break;
             } else {
-                System.out.print("\f      FINNZ DATING SERVICE\n\nList of names:\n");
-                printArray(nameList);      
-                System.out.println();
+                printInfo(nameList);  
                 System.out.print("The name can only be one in the list!\n\n");
             }
         }
-        System.out.print("\f      FINNZ DATING SERVICE\n\nList of names:\n");    
-        nameList.add("Jenifer Lopez");
-        printArray(nameList);
-        System.out.println();
+        nameList.add("jennifer lopez");
+        printInfo(nameList);  
 
         while (true) {
             System.out.print("Finn, which cousin has the best teeth? ");
@@ -78,37 +71,57 @@ public class FinnzDatingService
             if (nameList.contains(response)) {
                 break;
             } else {
-                System.out.print("\f      FINNZ DATING SERVICE\n\nList of names:\n");
-                printArray(nameList);      
-                System.out.println();
+                printInfo(nameList);  
                 System.out.print("The name can only be one in the list!\n\n");
             }
         }
 
-        System.out.print("\f      FINNZ DATING SERVICE\n\nList of names:\n");
-        printArray(nameList);   
-        System.out.println();
+        printInfo(nameList);
 
         while (true) {
             System.out.print("What is your sisters name? ");
             String response2 = input.nextLine();
-            if (response2.toLowerCase().matches("^[\\p{L} .'-]+$")) {
+            if (response2.toLowerCase().matches("^[\\p{L} .'-]+$") && !(nameList.contains(response2))) {
                 nameList.set(nameList.indexOf(response), response2);
-
                 break;
-            } else {
-                System.out.print("\f        FINNZ DATING SERVICE\n\nThe name can only contain letters!\n\n");
+            } else {    
+                printInfo(nameList); 
+                System.out.print("The name can only contain letters and cannot be on the list!\n\n");                
             }   
-
         }
-        
-        
+
+        printInfo(nameList);  
+
+        for (int i = 0; i < Integer.parseInt(numCousins)*2; i = i+2) {
+            while (true) {
+                printInfo(nameList);
+                System.out.print("How old is " + nameList.get(i) + "? ");
+                String age = input.nextLine();
+                if (age.toLowerCase().matches("^[0-9]*$")) {
+                    nameList.add(nameList.indexOf(nameList.get(i))+1, age);
+                    System.out.print("\f      FINNZ DATING SERVICE\n\n");
+                    break;
+                } else {
+                    System.out.print("\f      FINNZ DATING SERVICE\n\nThe age must be a number!\n\n");
+                }   
+            }
+        }
+        printInfo(nameList);
     }
 
     public static void printArray(ArrayList<String> array) {
         for (String names: array) {
-            System.out.print(names + "\n");
+            if (names.matches("^[0-9]*$")) {
+                System.out.print(" - Age: " + names);
+            } else {
+                System.out.print("\n" + names);
+            }
         }
     }
-}
 
+    public static void printInfo(ArrayList<String> nameList) {
+        System.out.print("\f      FINNZ DATING SERVICE\n\nList of names:");
+        printArray(nameList);   
+        System.out.println("\n"); 
+    }
+}
